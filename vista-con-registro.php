6e65cc258168034559c4ecfe198ac9b03a8e3d3c
franjavi01 <fjsg0001@gmail.com>
2022-01-09 18:29:13 +0100

    vista con registro desde formulario de entrada

--- vista-con-registro.php
+++ vista-con-registro.php
@@ -1,14 +1,27 @@
 <?php
 
-echo "Usuario: <strong>" . $_POST['user'] . "</strong><br>"
+$user = $_POST['user'];
+$password1 = $_POST['password1'];
+
+$conexion = mysqli_connect("localhost", "root", "", "canal+");
+$consulta = "SELECT * FROM registro WHERE user = '$user' and password1='$password1'";
+$result = mysqli_query($conexion, $consulta);
+$filas = mysqli_num_rows($result);
+
+if ($filas>0)
+{
+    echo "Usuario: <strong>" . $user . "</strong><br>"
 ?>
 <a href='./vista-sin-registro.php'>Finalizar sesión</a>
 <?php
-echo  "<hr>";
-echo  "<video src='./assets/video-sin-codificar.mp4' autoplay loop></video>";
+    echo  "<hr>";
+    echo  "<video src='./assets/video-sin-codificar.mp4' autoplay loop></video>";
+} else {
+    echo "Usuario o contraseña no coincide<br>" 
+?>
+    <a href='./formulario-entrada.php'>Vuelve a intentarlo</a>"
+<?php } 
 
-
-echo "Usuario o contraseña no coincide. <a href='./formulario-entrada.php'>Vuelve a intentarlo</a>";
- 
- 
- ?>   
+mysqli_free_result($result);
+mysqli_close($conexion);
+?>    

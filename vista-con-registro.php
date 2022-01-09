2466eeb4fd5137c9f3cf8c6fd6e22977f3d78217
franjavi01 <fjsg0001@gmail.com>
2022-01-09 07:11:41 +0100

    añadir finalizar sesión en la vista con registro

--- vista-con-registro.php
+++ vista-con-registro.php
@@ -1,9 +1,12 @@
 <?php
 
-echo "Usuario: <strong>" . $_POST['user'];
+echo "Usuario: <strong>" . $_POST['user'] . "</strong><br>"
+?>
+<a href='./vista-sin-registro.php'>Finalizar sesión</a>
+<?php
 echo  "<hr>";
 echo  "<video src='./assets/video-sin-codificar.mp4' autoplay loop></video>";
 
 
 echo "Usuario o contraseña no coincide. <a href='./formulario-entrada.php'>Vuelve a intentarlo</a>";
-    
+ ?>   
